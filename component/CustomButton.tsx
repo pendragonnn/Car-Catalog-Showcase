39a974e55c5a92b4772f5a216eb0ec4c216ea294
Wisnu Andika <124800648+pendragonnn@users.noreply.github.com>
2025-08-11 14:34:33 +0700

    feat: add hero section

---
+++ component/CustomButton.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { CustomButtonProps } from '@/types';
+import Image from 'next/image'
+import React from 'react'
+
+const CustomButton = ({ title, containerStyles, handleClick }: CustomButtonProps) => {
+  return (
+    <button 
+      disabled={false}
+      type = {"button"}
+      className={`flex flex-row relative justify-center items-center py-3 px-6 outline-none ${containerStyles}`}
+      onClick={handleClick} 
+    >
+      <span className={'flex-1'}>
+        {title}
+      </span>
+
+    </button>
+  )
+}
+
+export default CustomButton
